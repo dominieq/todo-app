0a110e94c47a23a279d89e38e125b30f1a156434
Dominik Szmyt <domino.onet.pl@op.pl>
2023-03-20 18:31:58 +0100

    Hide DELETE methods

--- src/main/java/io/github/dominieq/todoapp/database/repository/TaskRepository.java
+++ src/main/java/io/github/dominieq/todoapp/database/repository/TaskRepository.java
@@ -3,8 +3,16 @@ package io.github.dominieq.todoapp.database.repository;
 import io.github.dominieq.todoapp.database.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "tasks", collectionResourceRel = "tasks")
 public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
 
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Integer integer);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(TaskEntity entity);
 }
