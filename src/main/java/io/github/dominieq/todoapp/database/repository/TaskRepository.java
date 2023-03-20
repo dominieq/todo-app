f91732e0c6a52d7a3a50d77878c71e6aea03eb0f
Dominik Szmyt <domino.onet.pl@op.pl>
2023-03-20 18:31:44 +0100

    Change REST path for tasks

--- src/main/java/io/github/dominieq/todoapp/database/repository/TaskRepository.java
+++ src/main/java/io/github/dominieq/todoapp/database/repository/TaskRepository.java
@@ -4,7 +4,7 @@ import io.github.dominieq.todoapp.database.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "tasks", collectionResourceRel = "tasks")
 public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
 
 }
