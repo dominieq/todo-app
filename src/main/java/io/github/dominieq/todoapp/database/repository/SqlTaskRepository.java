0484fdc99d41eb000dcec5dac9e237705276ce8b
Dominik Szmyt <domino.onet.pl@op.pl>
2023-03-28 20:11:34 +0200

    Create custom query for existsById

--- src/main/java/io/github/dominieq/todoapp/database/repository/SqlTaskRepository.java
+++ src/main/java/io/github/dominieq/todoapp/database/repository/SqlTaskRepository.java
@@ -2,9 +2,14 @@ package io.github.dominieq.todoapp.database.repository;
 
 import io.github.dominieq.todoapp.database.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<TaskEntity, Integer> {
 
+	@Query(nativeQuery = true, value = "SELECT COUNT(*) > 0 FROM TASKS WHERE ID=:id")
+	@Override
+	boolean existsById(@Param("id") Integer id);
 }
