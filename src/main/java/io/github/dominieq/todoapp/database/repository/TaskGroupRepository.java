135c61a5e8b386e18356c5185776f20c00ead5a9
Dominik Szmyt <domino.onet.pl@op.pl>
2023-03-29 18:55:48 +0200

    Create repository for TaskGroupEntity

---
+++ src/main/java/io/github/dominieq/todoapp/database/repository/TaskGroupRepository.java
@@ -0,0 +1,15 @@
+package io.github.dominieq.todoapp.database.repository;
+
+import io.github.dominieq.todoapp.database.entity.TaskGroupEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskGroupRepository {
+
+	List<TaskGroupEntity> findAll();
+
+	Optional<TaskGroupEntity> findById(Integer id);
+
+	TaskGroupEntity save(TaskGroupEntity entity);
+}
---
+++ src/main/java/io/github/dominieq/todoapp/database/repository/jpa/SqlTaskGroupRepository.java
@@ -0,0 +1,17 @@
+package io.github.dominieq.todoapp.database.repository.jpa;
+
+import io.github.dominieq.todoapp.database.entity.TaskGroupEntity;
+import io.github.dominieq.todoapp.database.repository.TaskGroupRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroupEntity, Integer> {
+
+	@Query("from TaskGroupEntity g join fetch g.tasks") // Solve 'n + 1 selects' problems
+	@Override
+	List<TaskGroupEntity> findAll();
+}
